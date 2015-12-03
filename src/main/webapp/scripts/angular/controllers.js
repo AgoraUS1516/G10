54fa9bc5b912dfe4666fd272e5d2c8de37df0a8c
Daniel Sánchez <dansanav@gmail.com>
2015-12-03 20:01:34 +0100

    MODIFICACION: Ya no usa Apache tiles
    Se han comentado las lineas en spring/config/servlet.xml para que la
    aplicaciÃ³n deje de usar Apache tiles.
    La pÃ¡gina principal que carga ahora es index.html (se ha modificado el
    "welcome-file" de web.xml.

--- src/main/webapp/scripts/angular/controllers.js
+++ src/main/webapp/scripts/angular/controllers.js
@@ -1,7 +1,8 @@
 angular.module('AgoraUS', [])
 
-	//FooterController
-	.controller('FooterController', ['$scope', function($scope){
+	//Header y Footer
+	.controller('MainController', ['$scope', function($scope){
 		$scope.date = new Date();
 		$scope.footer = {name: "footer.html", url:"views/main/footer.html"};
+		$scope.header = {name: "header.html", url:"views/main/header.html"};
 	}]);
