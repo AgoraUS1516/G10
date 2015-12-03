604ab267a9b656a9ca374211408eb53fa0a1af29
Daniel Sánchez <dansanav@gmail.com>
2015-12-03 23:32:51 +0100

    ADICION: Mapeo de vistas con ajax
    Ahora se aÃ±aden vistas al <div ng-view></div> de index.html a traves del
    controlador de angular

--- src/main/webapp/scripts/angular/controllers.js
+++ src/main/webapp/scripts/angular/controllers.js
@@ -1,8 +1,28 @@
-angular.module('AgoraUS', [])
+var agoraUSControllers = angular.module('AgoraUSControllers', ['ngRoute']);	
 
-	//Header y Footer
-	.controller('MainController', ['$scope', function($scope){
-		$scope.date = new Date();
-		$scope.footer = {name: "footer.html", url:"views/main/footer.html"};
-		$scope.header = {name: "header.html", url:"views/main/header.html"};
-	}]);
+function showHeaderAndFooter($scope){
+	$scope.date = new Date();
+	$scope.footer = {name: "footer.html", url:"views/main/footer.html"};
+	$scope.header = {name: "header.html", url:"views/main/header.html"};
+}
+//Header y Footer
+agoraUSControllers.controller('MainController', ['$scope', '$route', '$routeParams', '$location', function($scope, $route, $routeParams, $location){
+	$scope.$route = $route;
+    $scope.$location = $location;
+    $scope.$routeParams = $routeParams;
+	showHeaderAndFooter($scope);
+}]);
+
+//Vista por defecto en index.html
+agoraUSControllers.controller('MainViewController',['$scope','$routeParams',function($scope,$routeParams){
+	showHeaderAndFooter($scope);
+	$scope.params = $routeParams;
+}]);
+
+//Visualización de resultados
+agoraUSControllers.controller('VisualizacionController', ['$scope','$routeParams', function($scope,$routeParams){
+	showHeaderAndFooter($scope);
+	$scope.encuestas = [];
+	$scope.test= "funciona";
+	$scope.params = $routeParams;
+}]);
