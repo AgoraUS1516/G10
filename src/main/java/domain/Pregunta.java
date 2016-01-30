e2392d29b29eca4bc8aa2f358ddecf08f5dfdcc4
MoisesLlamasGarcía <moisesllamasgarcia@gmailcom>
2016-01-30 10:48:13 +0100

    ADICIÓN: Añadida la nueva clase Pregunta, correspondiente al JSON nuevo y añadido el nuevo populate, representando a la nueva forma del json

---
+++ src/main/java/domain/Pregunta.java
@@ -0,0 +1,66 @@
+package domain;
+
+import java.util.Collection;
+
+import javax.persistence.Access;
+import javax.persistence.AccessType;
+import javax.persistence.Entity;
+import javax.persistence.ManyToOne;
+import javax.persistence.OneToMany;
+
+import org.hibernate.validator.constraints.NotBlank;
+import org.hibernate.validator.constraints.SafeHtml;
+import org.hibernate.validator.constraints.SafeHtml.WhiteListType;
+
+
+@Entity
+@Access(AccessType.PROPERTY)
+public class Pregunta extends DomainEntity{
+	
+	public Pregunta(){
+		super();
+		
+	}
+	
+	String pregunta;
+	String cp;
+	
+	@SafeHtml(whitelistType = WhiteListType.NONE)
+	@NotBlank
+	public String getPregunta() {
+		return pregunta;
+	}
+	public void setPregunta(String pregunta) {
+		this.pregunta = pregunta;
+	}
+	
+	@SafeHtml(whitelistType = WhiteListType.NONE)
+	@NotBlank
+	public String getCp() {
+		return cp;
+	}
+	public void setCp(String cp) {
+		this.cp = cp;
+	}
+	
+	private Encuesta encuesta;
+	private Collection<Opcion> opcions;
+
+	
+	@ManyToOne(optional=false)
+	public Encuesta getEncuesta() {
+		return encuesta;
+	}
+	public void setEncuesta(Encuesta encuesta) {
+		this.encuesta = encuesta;
+	}
+	
+	@OneToMany(mappedBy="pregunta")
+	public Collection<Opcion> getOpcions() {
+		return opcions;
+	}
+	public void setOpcions(Collection<Opcion> opcions) {
+		this.opcions = opcions;
+	}
+
+}
