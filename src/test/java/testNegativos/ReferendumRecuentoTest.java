fbda26f7c1b0deafd97695cf19fed61339206dab
valbarsau <valentinbarquero@gmail.com>
2016-02-08 00:51:04 +0100

    MODIFICACIÓN: Comentado el @Test y desactivado el populate ya que se producen errores
    Se producen errores nuevos en Jenkins al no existir la tabla, usar
    script sql.

--- src/test/java/testNegativos/ReferendumRecuentoTest.java
+++ src/test/java/testNegativos/ReferendumRecuentoTest.java
@@ -1,70 +1,70 @@
-//package testNegativos;
-//
-//import java.util.Collection;
-//
-//import org.junit.Test;
-//import org.junit.runner.RunWith;
-//import org.springframework.beans.factory.annotation.Autowired;
-//import org.springframework.test.context.ContextConfiguration;
-//import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
-//import org.springframework.test.context.transaction.TransactionConfiguration;
-//import org.springframework.transaction.annotation.Transactional;
-//import org.springframework.util.Assert;
-//
-//import domain.Propuesta;
-//import domain.ReferendumRecuento;
-//
-//import services.PropuestaService;
-//import services.ReferendumRecuentoService;
-//import utilities.AbstractTest;
-//
-//
+package testNegativos;
+
+import java.util.Collection;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+import org.springframework.test.context.transaction.TransactionConfiguration;
+import org.springframework.transaction.annotation.Transactional;
+import org.springframework.util.Assert;
+
+import domain.Propuesta;
+import domain.ReferendumRecuento;
+
+import services.PropuestaService;
+import services.ReferendumRecuentoService;
+import utilities.AbstractTest;
+
+
 //@ContextConfiguration(locations = { "classpath:spring/datasource.xml",
 //"classpath:spring/config/packages.xml" })
 //@RunWith(SpringJUnit4ClassRunner.class)
 //@Transactional
 //@TransactionConfiguration(defaultRollback = true)
-//public class ReferendumRecuentoTest extends AbstractTest{
-//
-//	
-//	String admin = "admin";
-//	
-//	
-//	@Autowired
-//	private ReferendumRecuentoService referendumRecuentoService;
-//	
-//	@Autowired
-//	private PropuestaService propuestaService;
-//	
-//	
-//	@Test(expected=NullPointerException.class)
-//	public void testReferendumFalse1(){
-//		authenticate(admin);
-//		
-//		ReferendumRecuento referendumRecuento;
-//		referendumRecuento = referendumRecuentoService.findIdVotacionModificacion(10);
-//		Assert.isTrue(referendumRecuento.getNombre()=="ReferendumInventado");
-//				
-//		unauthenticate();
-//	}
-//	
-//	@Test(expected=IllegalArgumentException.class)
-//	public void testReferendumFalse2(){
-//		authenticate(admin);
-//		
-//		Propuesta propuesta;
-//		propuesta = propuestaService.find(5);
-//		
-//		ReferendumRecuento referendumRecuento;
-//		referendumRecuento = referendumRecuentoService.getReferemdumById(9);
-//		
-//		Collection<Propuesta> propuestas;
-//		propuestas = referendumRecuento.getPropuestas();
-//		
-//		Assert.isTrue(propuestas.contains(propuesta));
-//		unauthenticate();
-//		
-//		
-//	}
-//
-//}
+public class ReferendumRecuentoTest extends AbstractTest{
+
+	
+	String admin = "admin";
+	
+	
+	@Autowired
+	private ReferendumRecuentoService referendumRecuentoService;
+	
+	@Autowired
+	private PropuestaService propuestaService;
+	
+	
+	//@Test(expected=NullPointerException.class)
+	public void testReferendumFalse1(){
+		authenticate(admin);
+		
+		ReferendumRecuento referendumRecuento;
+		referendumRecuento = referendumRecuentoService.findIdVotacionModificacion(10);
+		Assert.isTrue(referendumRecuento.getNombre()=="ReferendumInventado");
+				
+		unauthenticate();
+	}
+	
+	//@Test(expected=IllegalArgumentException.class)
+	public void testReferendumFalse2(){
+		authenticate(admin);
+		
+		Propuesta propuesta;
+		propuesta = propuestaService.find(5);
+		
+		ReferendumRecuento referendumRecuento;
+		referendumRecuento = referendumRecuentoService.getReferemdumById(9);
+		
+		Collection<Propuesta> propuestas;
+		propuestas = referendumRecuento.getPropuestas();
+		
+		Assert.isTrue(propuestas.contains(propuesta));
+		unauthenticate();
+		
+		
+	}
+
+}
--- src/test/java/testNegativos/TestPropuesta.java
+++ src/test/java/testNegativos/TestPropuesta.java
@@ -1,102 +1,102 @@
-//package testNegativos;
-//
-//import java.util.Collection;
-//import java.util.HashSet;
-//
-//import org.junit.Test;
-//import org.junit.runner.RunWith;
-//import org.springframework.beans.factory.annotation.Autowired;
-//import org.springframework.test.context.ContextConfiguration;
-//import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
-//import org.springframework.test.context.transaction.TransactionConfiguration;
-//import org.springframework.transaction.annotation.Transactional;
-//import org.springframework.util.Assert;
-//
-//import domain.Propuesta;
-//import domain.ReferendumRecuento;
-//
-//import services.PropuestaService;
-//import services.ReferendumRecuentoService;
-//import utilities.AbstractTest;
-//
+package testNegativos;
+
+import java.util.Collection;
+import java.util.HashSet;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+import org.springframework.test.context.transaction.TransactionConfiguration;
+import org.springframework.transaction.annotation.Transactional;
+import org.springframework.util.Assert;
+
+import domain.Propuesta;
+import domain.ReferendumRecuento;
+
+import services.PropuestaService;
+import services.ReferendumRecuentoService;
+import utilities.AbstractTest;
+
 //
 //@ContextConfiguration(locations = { "classpath:spring/datasource.xml",
 //"classpath:spring/config/packages.xml" })
 //@RunWith(SpringJUnit4ClassRunner.class)
 //@Transactional
 //@TransactionConfiguration(defaultRollback = true)
-//public class TestPropuesta extends AbstractTest{
-//	
-//	String admin = "admin";
-//	
-//	
-//	@Autowired
-//	private ReferendumRecuentoService referendumRecuentoService;
-//	
-//	@Autowired
-//	private PropuestaService propuestaService;
-//	
-//	
-//	
+public class TestPropuesta extends AbstractTest{
+	
+	String admin = "admin";
+	
+	
+	@Autowired
+	private ReferendumRecuentoService referendumRecuentoService;
+	
+	@Autowired
+	private PropuestaService propuestaService;
+	
+	
+	
+	//@Test(expected=IllegalArgumentException.class)
+	public void testFindPropuestaSize() {
+		authenticate(admin);
+	
+		Collection<Propuesta> all=new HashSet<Propuesta>();
+		all = propuestaService.findAll();
+		Assert.isTrue(all.size() == 6);
+		
+		authenticate(null);
+	}
+	
+	//@Test(expected=IllegalArgumentException.class)
+	public void testFindPropuestaFalsa(){
+	authenticate(admin);
+	
+	ReferendumRecuento referendumRecuento;
+	referendumRecuento = referendumRecuentoService.findIdVotacionModificacion(1);
+	Collection<Propuesta> propuestas;
+	propuestas = referendumRecuento.getPropuestas();
+	
+	Propuesta propuesta;
+	propuesta = propuestaService.find(12);
+	
+	Assert.isTrue(propuestas.contains(propuesta));
+	authenticate(null);
+	}
+	
+	//@Test(expected=IllegalArgumentException.class)
+	public void testFindPropuestaFalsa2(){
+		
+		authenticate(admin);
+		Propuesta propuesta;
+		propuesta = propuestaService.find(9);
+		
+		
+		ReferendumRecuento referendumRecuento;
+		referendumRecuento = referendumRecuentoService.findIdVotacionModificacion(1);
+		Collection<Propuesta> propuestas;
+		propuestas = referendumRecuento.getPropuestas();
+		Assert.isTrue(propuestas.contains(propuesta));
+		unauthenticate();
+		
+	}
+	
+	
 //	@Test(expected=IllegalArgumentException.class)
-//	public void testFindPropuestaSize() {
-//		authenticate(admin);
-//	
-//		Collection<Propuesta> all=new HashSet<Propuesta>();
-//		all = propuestaService.findAll();
-//		Assert.isTrue(all.size() == 6);
-//		
-//		authenticate(null);
-//	}
-//	
-//	@Test(expected=IllegalArgumentException.class)
-//	public void testFindPropuestaFalsa(){
+//	public void testFindPropuestaFalsa2(){
 //	authenticate(admin);
 //	
-//	ReferendumRecuento referendumRecuento;
-//	referendumRecuento = referendumRecuentoService.findIdVotacionModificacion(1);
-//	Collection<Propuesta> propuestas;
-//	propuestas = referendumRecuento.getPropuestas();
-//	
+//	Collection<Propuesta>propuestas;
+//	propuestas = propuestaService.findAll();
 //	Propuesta propuesta;
-//	propuesta = propuestaService.find(12);
 //	
 //	Assert.isTrue(propuestas.contains(propuesta));
 //	authenticate(null);
 //	}
-//	
-//	@Test(expected=IllegalArgumentException.class)
-//	public void testFindPropuestaFalsa2(){
-//		
-//		authenticate(admin);
-//		Propuesta propuesta;
-//		propuesta = propuestaService.find(9);
-//		
-//		
-//		ReferendumRecuento referendumRecuento;
-//		referendumRecuento = referendumRecuentoService.findIdVotacionModificacion(1);
-//		Collection<Propuesta> propuestas;
-//		propuestas = referendumRecuento.getPropuestas();
-//		Assert.isTrue(propuestas.contains(propuesta));
-//		unauthenticate();
-//		
-//	}
-//	
-//	
-////	@Test(expected=IllegalArgumentException.class)
-////	public void testFindPropuestaFalsa2(){
-////	authenticate(admin);
-////	
-////	Collection<Propuesta>propuestas;
-////	propuestas = propuestaService.findAll();
-////	Propuesta propuesta;
-////	
-////	Assert.isTrue(propuestas.contains(propuesta));
-////	authenticate(null);
-////	}
-//	
-//	
-//	
-//	
-//	
-//}
+	
+	
+	
+	
+	
+}
